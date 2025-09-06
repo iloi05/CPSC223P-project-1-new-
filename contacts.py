681e7c54afe8245f5e3f6e7b2d4550b7cb729c41
Ivy <iloi05@csu.fullerton.edu>
2025-09-05 22:39:52 -0700

    Add files via upload

---
+++ contacts.py
@@ -0,0 +1,10 @@
+# Name: Ivy Loi
+# Date: 9/4/25
+# Purpose: Code that contians a list of contacts that can be modified or deleted
+
+contact_list = ["first name", "last name"]
+def print_list(contact_list):
+    """ Holds first and last names after inputting into the contact list """
+    print("Index   First Name   Last Name")
+    for c in contact_list:
+        print(first_name, last_name)
---
+++ main.py
@@ -0,0 +1,9 @@
+# Name: ivy Loi
+# Date: 9/4/25
+
+print ("*** EMPLOYEE CONTACT MAIN MENU")
+print ("1. Print list")
+print ("2. Add contact")
+print ("3. Modify contact")
+print ("4. Delete contact")
+print ("5. Exit the program")
